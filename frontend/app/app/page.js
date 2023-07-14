e631459d4cdda885180071a53b775111d486e204
chandrabosep <chandrabosep3112@gmail.com>
2023-07-14 23:56:27 +0530

    added scroll trigger

--- frontend/app/app/page.js
+++ frontend/app/app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import LocomotiveScroll from 'locomotive-scroll';
+import React, { useRef,useEffect } from "react";
 
 export default function Home() {
+  
+
+  const ref = useRef(null);
+
+  useEffect(()=>{
+    let locoScroll;
+    locoScroll = new LocomotiveScroll({
+    el: ref.current,
+    smooth: true,
+    });
+    new ResizeObserver(() => locoScroll.update()).observe(
+      ref.current
+    );
+    },[]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center overflow-hidden bg-black py-12 text-[#dff896]">
+    <main ref={ref} className="flex min-h-screen flex-col items-center overflow-hidden bg-black py-12 text-[#dff896]">
       <nav className="w-11/12 border-y-2 border-[#dff896] py-6 flex flex-col lg:flex-row lg:justify-between  items-center text-center gap-6 ">
         <span>SupplyX</span>
         <ul className="gap-6 flex flex-col lg:flex-row">
@@ -38,15 +55,14 @@ export default function Home() {
         <Image
           src="/img1.png"
           alt="product warehouse"
-          layout="responsive"
-          width={0}
+          width={4000}
           height={0}
           className="h-1/4 overflow-clip bg-cover "
         />
         
       </div>
-      <div className="w-11/12 lg:flex relative lg:flex-col lg:w-8/12 text-left lg:gap-10 lg:py-20">
-        <h2 className="z-10 font-semibold text-[6dvw] lg:text-[3.2dvw] lg:font-bold lg:w-9/12 text-[#cbff2d] opacity-40 pb-3">
+      <div className="w-11/12 lg:flex relative lg:flex-col lg:w-8/12 text-left lg:gap-10 lg:py-20 py-14">
+        <h2 className="z-10 font-semibold text-[6dvw] lg:text-[3.2dvw] lg:font-bold lg:w-9/12 text-[#dff896] opacity-80 pb-3">
           Two Categories, One Platform
         </h2>
         <p className="z-10 text-slate-50 lg:w-4/6 lg:text-[1.5rem] lg:font-medium">
@@ -56,7 +72,7 @@ export default function Home() {
         </p>
         <div className="lg:z-0 lg:w-[50rem] lg:self-end  lg:h-[25rem] lg:absolute lg:border lg:-right-48 lg:-top-20 lg:-rotate-45 lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
       </div>
-      <div className="relative w-9/12 py-20 flex flex-col gap-16 lg:flex-row lg:items-center lg:justify-between lg:pb-64">
+      <div className="relative w-9/12 p-20 flex flex-col gap-16 lg:flex-row lg:items-center lg:justify-between lg:pb-64">
         <div className="flex flex-col items-center text-center gap-4">
           <Image
             src="/team1.webp "
@@ -64,8 +80,7 @@ export default function Home() {
             height={50}
             className="rounded-full h-20 grayscale"
           />
-          <h3 className="font-semibold text-2xl">PETER</h3>
-          <p className="text-slate-50 text-lg font-light">Manufacturer</p>
+          <h3 className="font-semibold text-2xl">Manufacturer</h3>
         </div>
 
         <div className="flex flex-col items-center text-center gap-4">
@@ -75,8 +90,7 @@ export default function Home() {
             height={50}
             className="rounded-full h-20 grayscale"
           />
-          <h3 className="font-semibold text-2xl">PETER</h3>
-          <p className="text-slate-50 text-lg font-light">Manufacturer</p>
+          <h3 className="font-semibold text-2xl">Customer</h3>
         </div>
 
         <div className="flex flex-col items-center text-center gap-4">
@@ -86,8 +100,7 @@ export default function Home() {
             height={50}
             className="rounded-full h-20 grayscale"
           />
-          <h3 className="font-semibold text-2xl">PETER</h3>
-          <p className="text-slate-50 text-lg font-light">Manufacturer</p>
+          <h3 className="font-semibold text-2xl">Retailer</h3>
         </div>
 
         <div className="flex flex-col items-center text-center gap-4">
@@ -97,8 +110,7 @@ export default function Home() {
             height={50}
             className="rounded-full h-20 grayscale  "
           />
-          <h3 className="font-semibold text-2xl">PETER</h3>
-          <p className="text-slate-50 text-lg font-light">Manufacturer</p>
+          <h3 className="font-semibold text-2xl">Data Analyst</h3>
         </div>
       </div>
 
@@ -108,7 +120,7 @@ export default function Home() {
           <p className="text-slate-50">Live onchain logistics tracking</p>
           <Image
             src="/card1.webp"
-            width={100}
+            width={4000}
             height={100}
             quality={100}
             className="lg:mt-5 grayscale w-full h-52 object-cover rounded-xl self-center m-auto"
@@ -119,7 +131,7 @@ export default function Home() {
           <p className="text-slate-50">Live onchain logistics tracking</p>
           <Image
             src="/card2.webp"
-            width={100}
+            width={4000}
             height={100}
             quality={100}
             className="lg:mt-5 grayscale w-full h-52 object-cover rounded-xl self-center m-auto"
@@ -130,7 +142,7 @@ export default function Home() {
           <p className="text-slate-50">Live onchain logistics tracking</p>
           <Image
             src="/card3.webp"
-            width={100}
+            width={4000}
             height={100}
             quality={100}
             className="lg:mt-5 grayscale w-full h-52 object-cover rounded-xl self-center m-auto"
@@ -141,7 +153,7 @@ export default function Home() {
           <p className="text-slate-50">Live onchain logistics tracking</p>
           <Image
             src="/card4.webp"
-            width={100}
+            width={4000}
             height={100}
             quality={100}
             className="lg:mt-5 grayscale w-full h-52 object-cover rounded-xl self-center m-auto"
@@ -149,22 +161,17 @@ export default function Home() {
         </div>
         <div className="lg:z-0 lg:w-[35rem] lg:self-start  lg:h-[15rem] lg:-left-32 lg:top-10 lg:absolute lg:border lg:rotate-45 lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
         <div className="lg:z-0 lg:w-[40rem] lg:self-end lg:-right-64 lg:h-[20rem] lg:-bottom-[0.5rem] lg:absolute lg:border lg:rotate-[130deg] lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
+        <div className="lg:z-0 lg:w-[22rem] lg:absolute lg:h-52 lg:border lg:self-end lg:-left-48 lg:-bottom-40 lg:rotate-[30deg] lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
+        <div className="lg:z-0 lg:w-[22rem] lg:absolute lg:h-52 lg:border lg:self-end lg:-left-48 lg:-bottom-40 lg:rotate-[80deg] lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
       </div>
-      <div className=" w-11/12 lg:w-4/12 flex flex-col lg:items-center text-center my-20 gap-6">
-        <h2 className="text-3xl font-bold">Get Started</h2>
-        <p className="text-[0.98rem]">
-          Don’t wait another minute! Dive into the world of SupplyX and
-          revolutionize your supply chain experience. Join us now!
-        </p>
-        <div className="w-full lg:w-fit bg-[#704e2e] text-slate-50 text-sm px-6 py-2 rounded-xl">
-          Sign up
-        </div>
-        <div className="lg:z-0 lg:w-[22rem] lg:absolute lg:h-52 lg:border lg:self-start lg:left-16 lg:rotate-[30deg] lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
-        <div className="lg:z-0 lg:w-[22rem] lg:absolute lg:h-52 lg:border lg:self-start lg:left-16 lg:rotate-[80deg] lg:rounded-[100%] lg:border-zinc-700	 lg:p-10"></div>
+      
+      <div className=" w-11/12 lg:w-6/12 flex flex-col lg:items-center text-center my-20 gap-6 lg:pt-10">
+        <h2 className="text-lg lg:text-3xl font-bold">Connecting Users - Manufacturers - Retailers</h2>
+        
       </div>
       <div>
         <p className="text-slate-50 text-base font-light">
-          © SupplyX, Transforming Industries
+          © EtherNinjas, Web3Conf India
         </p>
       </div>
     </main>
