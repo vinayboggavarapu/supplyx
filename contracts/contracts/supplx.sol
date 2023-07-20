35c3d33bc0020285857c65999afc08254bbb853d
Dusa Praharsh <dusapraharsh123@gmail.com>
2023-07-20 13:05:25 +0530

    multi fix

--- contracts/contracts/supplx.sol
+++ contracts/contracts/supplx.sol
@@ -2,102 +2,138 @@
 pragma solidity ^0.8.0;
 
 contract SupplyX {
-    address public retailerAddress; // retailer address
-    address public manufacturerAddress; // manufacturer address
-    address[] public preOrderBuyers;
-
-    mapping(address => uint256) public escrowBalances;
-    mapping(address => bool) public retailerConfirmed;
-    mapping(address => bool) public manufacturerConfirmed;
-    mapping(address => bool) public hasPlacedOrder;
-
-    uint256 public unitPrice; // price of the product
-
-    event PreOrderPlaced(address indexed buyer, uint256 quantity);
-    event OrderConfirmed(address indexed retailer, address indexed manufacturer, uint256 quantity);
-    event OrderShipped(address indexed retailer, address indexed manufacturer, uint256 quantity);
-    event PaymentReleased(address indexed buyer, address indexed retailer, address indexed manufacturer, uint256 rawMaterialCost, uint256 remainingPayment);
-
-    constructor(address _retailerAddress, address _manufacturerAddress, uint256 _unitPrice) {
-        retailerAddress = _retailerAddress;
-        manufacturerAddress = _manufacturerAddress;
-        unitPrice = _unitPrice;
+    struct Order {
+        uint256 quantity;
+        uint256 escrowBalance;
+        bool retailerConfirmed;
+        bool manufacturerConfirmed;
     }
 
+    mapping(address => Order) public orders;
+    mapping(address => address[]) public retailerPreOrderBuyers;
+    mapping(address => address[]) public manufacturerPreOrderBuyers;
+
+    mapping(address => bool) public isRetailer;
+    mapping(address => bool) public isManufacturer;
+
+    event PreOrderPlaced(address indexed buyer, uint256 quantity);
+    event OrderConfirmed(address indexed retailer, address indexed manufacturer, address indexed buyer, uint256 quantity);
+    event OrderShipped(address indexed retailer, address indexed manufacturer, address indexed buyer, uint256 quantity);
+    event PaymentReleased(address indexed buyer, address indexed retailer, address indexed manufacturer, uint256 rawMaterialCost, uint256 remainingPayment);
+
     modifier onlyRetailer() {
-        require(msg.sender == retailerAddress, "Only retailer can call this function.");
-        _; // retailer confirmation functionality
+        require(isRetailer[msg.sender], "Only retailer can call this function.");
+        _;
     }
 
     modifier onlyManufacturer() {
-        require(msg.sender == manufacturerAddress, "Only manufacturer can call this function.");
-        _; // manufacturer confirmation functionality
+        require(isManufacturer[msg.sender], "Only manufacturer can call this function.");
+        _;
+    }
+
+    function addRetailer(address _retailerAddress) external {
+        require(!isRetailer[_retailerAddress], "Retailer already exists.");
+        isRetailer[_retailerAddress] = true;
+    }
+
+    function addManufacturer(address _manufacturerAddress) external {
+        require(!isManufacturer[_manufacturerAddress], "Manufacturer already exists.");
+        isManufacturer[_manufacturerAddress] = true;
     }
 
     function placeOrder(uint256 _quantity) external payable {
-        require(!hasPlacedOrder[msg.sender], "Wallet has already placed an order.");
-        require(msg.value >= unitPrice * _quantity, "Insufficient payment.");
+        require(!orders[msg.sender].retailerConfirmed && !orders[msg.sender].manufacturerConfirmed, "Order already placed or confirmed.");
+        require(msg.value >= orders[msg.sender].quantity * _quantity, "Insufficient payment.");
 
-        escrowBalances[msg.sender] += msg.value;
-        preOrderBuyers.push(msg.sender);
-        hasPlacedOrder[msg.sender] = true;
+        orders[msg.sender].quantity = _quantity;
+        orders[msg.sender].escrowBalance = msg.value;
+
+        address retailerAddress = getRetailerFor(msg.sender);
+        address manufacturerAddress = getManufacturerFor(msg.sender);
+
+        retailerPreOrderBuyers[retailerAddress].push(msg.sender);
+        manufacturerPreOrderBuyers[manufacturerAddress].push(msg.sender);
+
         emit PreOrderPlaced(msg.sender, _quantity);
     }
 
     function confirmOrder() external onlyRetailer {
+        address[] memory preOrderBuyers = retailerPreOrderBuyers[msg.sender];
         require(preOrderBuyers.length > 0, "No pre-orders available.");
-        require(!retailerConfirmed[msg.sender], "Order already confirmed by this retailer.");
+        require(!orders[msg.sender].retailerConfirmed, "Order already confirmed by this retailer.");
 
-        retailerConfirmed[msg.sender] = true;
+        orders[msg.sender].retailerConfirmed = true;
 
-        emit OrderConfirmed(retailerAddress, manufacturerAddress, escrowBalances[preOrderBuyers[0]]);
+        emit OrderConfirmed(msg.sender, getManufacturerFor(msg.sender), preOrderBuyers[0], orders[preOrderBuyers[0]].quantity);
 
-        if (allConfirmed()) {
-            releasePayment();
+        if (allConfirmed(preOrderBuyers)) {
+            releasePayment(preOrderBuyers);
         }
     }
 
     function confirmShipment() external onlyManufacturer {
+        address[] memory preOrderBuyers = manufacturerPreOrderBuyers[msg.sender];
         require(preOrderBuyers.length > 0, "No pre-orders available.");
-        require(retailerConfirmed[retailerAddress], "Retailer has not confirmed the shipment.");
-        require(!manufacturerConfirmed[msg.sender], "Order already confirmed by this manufacturer.");
+        require(orders[getRetailerFor(msg.sender)].retailerConfirmed, "Retailer has not confirmed the shipment.");
+        require(!orders[msg.sender].manufacturerConfirmed, "Order already confirmed by this manufacturer.");
 
-        manufacturerConfirmed[msg.sender] = true;
+        orders[msg.sender].manufacturerConfirmed = true;
 
-        emit OrderShipped(retailerAddress, manufacturerAddress, escrowBalances[preOrderBuyers[0]]);
+        emit OrderShipped(getRetailerFor(msg.sender), msg.sender, preOrderBuyers[0], orders[preOrderBuyers[0]].quantity);
 
-        if (allConfirmed()) {
-            releasePayment();
+        if (allConfirmed(preOrderBuyers)) {
+            releasePayment(preOrderBuyers);
         }
     }
 
-    function allConfirmed() internal view returns (bool) {
-        if (preOrderBuyers.length == 0) {
+    function allConfirmed(address[] memory _preOrderBuyers) internal view returns (bool) {
+        if (_preOrderBuyers.length == 0) {
             return false;
         }
 
-        for (uint256 i = 0; i < preOrderBuyers.length; i++) {
-            address buyer = preOrderBuyers[i];
-            if (!retailerConfirmed[retailerAddress] || !manufacturerConfirmed[manufacturerAddress]) {
+        for (uint256 i = 0; i < _preOrderBuyers.length; i++) {
+            address buyer = _preOrderBuyers[i];
+            if (!orders[buyer].retailerConfirmed || !orders[buyer].manufacturerConfirmed) {
                 return false;
             }
         }
         return true;
     }
 
-    function releasePayment() internal {
-        for (uint256 i = 0; i < preOrderBuyers.length; i++) {
-            address buyer = preOrderBuyers[i];
-            uint256 totalEscrowAmount = escrowBalances[buyer];
-            escrowBalances[buyer] = 0;
+    function releasePayment(address[] memory _preOrderBuyers) internal {
+        for (uint256 i = 0; i < _preOrderBuyers.length; i++) {
+            address buyer = _preOrderBuyers[i];
+            uint256 totalEscrowAmount = orders[buyer].escrowBalance;
+            orders[buyer].escrowBalance = 0;
 
             uint256 rawMaterialCost = totalEscrowAmount / 2;
             uint256 remainingPayment = totalEscrowAmount - rawMaterialCost;
+
+            address retailerAddress = getRetailerFor(buyer);
+            address manufacturerAddress = getManufacturerFor(buyer);
 
             payable(retailerAddress).transfer(rawMaterialCost);
             payable(manufacturerAddress).transfer(remainingPayment);
 
             emit PaymentReleased(buyer, retailerAddress, manufacturerAddress, rawMaterialCost, remainingPayment);
         }
+    }
+
+    function getRetailerFor(address _buyer) internal view returns (address) {
+        for (uint256 i = 0; i < retailerPreOrderBuyers[msg.sender].length; i++) {
+            if (retailerPreOrderBuyers[msg.sender][i] == _buyer) {
+                return msg.sender;
+            }
+        }
+        revert("Retailer not found.");
+    }
+
+    function getManufacturerFor(address _buyer) internal view returns (address) {
+        for (uint256 i = 0; i < manufacturerPreOrderBuyers[msg.sender].length; i++) {
+            if (manufacturerPreOrderBuyers[msg.sender][i] == _buyer) {
+                return msg.sender;
+            }
+        }
+        revert("Manufacturer not found.");
     }
 }
