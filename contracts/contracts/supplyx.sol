54d934355ca43ed348b60ec1f4a86c5102aa4635
Vinay <vinayb950529@gmail.com>
2023-07-25 13:00:32 +0530

    update contract

--- contracts/contracts/supplyx.sol
+++ contracts/contracts/supplyx.sol
@@ -1,139 +1,247 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
+
+//contract
 
 contract SupplyX {
     struct Order {
         uint256 quantity;
         uint256 escrowBalance;
-        bool retailerConfirmed;
+        address manufacturer;
         bool manufacturerConfirmed;
+        bool orderCompleted;
+        bool highPriority;
     }
 
-    mapping(address => Order) public orders;
-    mapping(address => address[]) public retailerPreOrderBuyers;
-    mapping(address => address[]) public manufacturerPreOrderBuyers;
+    address[] public retailers;
+    address[] public manufacturers;
 
+    mapping(address => Order[]) public userOrders;
     mapping(address => bool) public isRetailer;
     mapping(address => bool) public isManufacturer;
+    mapping(address => mapping(address => Order[])) public manufacturerOrders;
+    mapping(address => mapping(address => uint256)) private totalPreOrderCount;
+    mapping(address => mapping(address => string))
+        private manufacturerPreOrderId;
 
-    event PreOrderPlaced(address indexed buyer, uint256 quantity);
-    event OrderConfirmed(address indexed retailer, address indexed manufacturer, address indexed buyer, uint256 quantity);
-    event OrderShipped(address indexed retailer, address indexed manufacturer, address indexed buyer, uint256 quantity);
-    event PaymentReleased(address indexed buyer, address indexed retailer, address indexed manufacturer, uint256 rawMaterialCost, uint256 remainingPayment);
+    // Mapping to check if manufacturer has set a order/preOrder for a specific retailer
+    mapping(address => mapping(address => bool)) public manufacturerSet;
 
-    modifier onlyRetailer() {
-        require(isRetailer[msg.sender], "Only retailer can call this function.");
+    // Mapping to check if retailer has confirmed the order/preOrder
+    mapping(address => mapping(address => bool)) public retailerConfirmed;
+
+    mapping(string => address) private _manufacturerSelectedCustomerAddress;
+
+    mapping(address => mapping(address => bool)) private hasPreOrder;
+
+    event OrderPlaced(
+        address indexed buyer,
+        uint256 quantity,
+        bool highPriority
+    );
+    event OrderConfirmedByManufacturer(
+        address indexed manufacturer,
+        address indexed retailer
+    );
+    event OrderConfirmedByRetailer(
+        address indexed retailer,
+        address indexed manufacturer
+    );
+    event CountAddedByManufacturer(
+        address indexed manufacturer,
+        address indexed retailer,
+        string count
+    );
+    event PriorityUpdated(
+        address indexed orderPlacer,
+        uint256 orderIndex,
+        bool highPriority
+    );
+    event PaymentReleased(
+        address indexed retailer,
+        address indexed manufacturer,
+        uint256 rawMaterialCost,
+        uint256 remainingPayment
+    );
+
+    event PreOrderConfirmed(address indexed customer, string preOrderId);
+
+    modifier onlyManufacturer() {
+        require(
+            isManufacturer[msg.sender],
+            "Only manufacturer can call this function."
+        );
         _;
     }
 
-    modifier onlyManufacturer() {
-        require(isManufacturer[msg.sender], "Only manufacturer can call this function.");
+    modifier onlyRetailer() {
+        require(
+            isRetailer[msg.sender],
+            "Only retailer can call this function."
+        );
         _;
     }
 
     function addRetailer(address _retailerAddress) external {
         require(!isRetailer[_retailerAddress], "Retailer already exists.");
         isRetailer[_retailerAddress] = true;
+        retailers.push(_retailerAddress);
     }
 
     function addManufacturer(address _manufacturerAddress) external {
-        require(!isManufacturer[_manufacturerAddress], "Manufacturer already exists.");
+        require(
+            !isManufacturer[_manufacturerAddress],
+            "Manufacturer already exists."
+        );
         isManufacturer[_manufacturerAddress] = true;
+        manufacturers.push(_manufacturerAddress);
     }
 
-    function placeOrder(uint256 _quantity) external payable {
-        require(!orders[msg.sender].retailerConfirmed && !orders[msg.sender].manufacturerConfirmed, "Order already placed or confirmed.");
-        require(msg.value >= orders[msg.sender].quantity * _quantity, "Insufficient payment.");
+    function preOrder(
+        address _manufacturer,
+        uint256 _quantity
+    ) external payable {
+        require(msg.value >= 0.005 ether * _quantity, "Insufficient payment.");
+        require(
+            !hasPreOrder[_manufacturer][msg.sender],
+            "Preorder already exists for this manufacturer."
+        );
 
-        orders[msg.sender].quantity = _quantity;
-        orders[msg.sender].escrowBalance = msg.value;
+        Order memory order = Order({
+            quantity: _quantity,
+            escrowBalance: msg.value,
+            manufacturer: _manufacturer,
+            manufacturerConfirmed: false,
+            orderCompleted: false,
+            highPriority: false
+        });
 
-        address retailerAddress = getRetailerFor(msg.sender);
-        address manufacturerAddress = getManufacturerFor(msg.sender);
+        manufacturerOrders[_manufacturer][msg.sender].push(order);
+        totalPreOrderCount[_manufacturer][msg.sender]++;
 
-        retailerPreOrderBuyers[retailerAddress].push(msg.sender);
-        manufacturerPreOrderBuyers[manufacturerAddress].push(msg.sender);
+        userOrders[msg.sender].push(order);
 
-        emit PreOrderPlaced(msg.sender, _quantity);
+        hasPreOrder[_manufacturer][msg.sender] = true;
+
+        emit OrderPlaced(msg.sender, _quantity, false);
     }
 
-    function confirmOrder() external onlyRetailer {
-        address[] memory preOrderBuyers = retailerPreOrderBuyers[msg.sender];
-        require(preOrderBuyers.length > 0, "No pre-orders available.");
-        require(!orders[msg.sender].retailerConfirmed, "Order already confirmed by this retailer.");
+    function updateOrderPriority(
+        address _manufacturer,
+        uint256 _orderIndex
+    ) external payable {
+        require(_manufacturer != address(0), "Invalid manufacturer address");
+        require(
+            _orderIndex < manufacturerOrders[_manufacturer][msg.sender].length,
+            "Invalid index."
+        );
+        require(
+            msg.value >= 0.001 ether,
+            "Minimum fee for priority update is 0.002 ether."
+        );
 
-        orders[msg.sender].retailerConfirmed = true;
+        Order storage order = userOrders[msg.sender][_orderIndex];
 
-        emit OrderConfirmed(msg.sender, getManufacturerFor(msg.sender), preOrderBuyers[0], orders[preOrderBuyers[0]].quantity);
+        require(
+            !order.manufacturerConfirmed,
+            "Cannot update the confirmed order."
+        );
+        order.highPriority = true;
 
-        if (allConfirmed(preOrderBuyers)) {
-            releasePayment(preOrderBuyers);
-        }
+        emit PriorityUpdated(msg.sender, _orderIndex, true);
     }
 
-    function confirmShipment() external onlyManufacturer {
-        address[] memory preOrderBuyers = manufacturerPreOrderBuyers[msg.sender];
-        require(preOrderBuyers.length > 0, "No pre-orders available.");
-        require(orders[getRetailerFor(msg.sender)].retailerConfirmed, "Retailer has not confirmed the shipment.");
-        require(!orders[msg.sender].manufacturerConfirmed, "Order already confirmed by this manufacturer.");
+    function addPreOrderCountForRetailer(
+        string memory preOrderId,
+        address retailer,
+        address user
+    ) external onlyManufacturer {
+        manufacturerPreOrderId[msg.sender][retailer] = preOrderId;
+        manufacturerSet[msg.sender][retailer] = true;
 
-        orders[msg.sender].manufacturerConfirmed = true;
+        _manufacturerSelectedCustomerAddress[preOrderId] = user;
 
-        emit OrderShipped(getRetailerFor(msg.sender), msg.sender, preOrderBuyers[0], orders[preOrderBuyers[0]].quantity);
-
-        if (allConfirmed(preOrderBuyers)) {
-            releasePayment(preOrderBuyers);
-        }
+        emit CountAddedByManufacturer(msg.sender, retailer, preOrderId);
     }
 
-    function allConfirmed(address[] memory _preOrderBuyers) internal view returns (bool) {
-        if (_preOrderBuyers.length == 0) {
-            return false;
-        }
+    function confirmPreOrderRetailer(
+        address _manufacturer
+    ) external onlyRetailer {
+        // Checking if the manufacturer has already set a preOrder for this retailer
+        require(
+            manufacturerSet[_manufacturer][msg.sender],
+            "Manufacturer has not set preOrder for this retailer."
+        );
 
-        for (uint256 i = 0; i < _preOrderBuyers.length; i++) {
-            address buyer = _preOrderBuyers[i];
-            if (!orders[buyer].retailerConfirmed || !orders[buyer].manufacturerConfirmed) {
-                return false;
+        retailerConfirmed[msg.sender][_manufacturer] = true;
+
+        emit OrderConfirmedByRetailer(msg.sender, _manufacturer);
+
+        // Resetting manufacturer set value after retailer confirmation.
+        manufacturerSet[_manufacturer][msg.sender] = false;
+    }
+
+    function getRetailers() external view returns (address[] memory) {
+        return retailers;
+    }
+
+    function getmanufacturers() external view returns (address[] memory) {
+        return manufacturers;
+    }
+
+    function confirmOrderManufacturer(
+        address _retailer
+    ) external onlyManufacturer {
+        string memory preOrderId = manufacturerPreOrderId[msg.sender][
+            _retailer
+        ];
+        // Ensuring the retailer has confirmed
+        require(
+            retailerConfirmed[_retailer][msg.sender],
+            "Order must be confirmed by retailer first."
+        );
+
+        address customer = _manufacturerSelectedCustomerAddress[preOrderId];
+
+        Order storage order = userOrders[customer][0];
+
+        if (!order.manufacturerConfirmed) {
+            emit OrderConfirmedByManufacturer(msg.sender, _retailer);
+
+            if (order.escrowBalance > 0) {
+                uint256 totalEscrowAmount = order.escrowBalance;
+                order.escrowBalance = 0;
+
+                uint256 rawMaterialCost = 0.001 ether;
+                uint256 remainingPayment = totalEscrowAmount - rawMaterialCost;
+
+                (bool transferToManufacturer, ) = payable(order.manufacturer)
+                    .call{value: rawMaterialCost}("");
+                (bool transferToRetailer, ) = payable(_retailer).call{
+                    value: remainingPayment
+                }("");
+
+                require(
+                    transferToManufacturer,
+                    "Transfer to manufacturer failed."
+                );
+                require(transferToRetailer, "Transfer to retailer failed.");
+
+                order.manufacturerConfirmed = true;
+                order.orderCompleted = true;
+
+                emit PreOrderConfirmed(customer, preOrderId);
+
+                // The remaining escrow balance of the order for the retailer is already updated above
+
+                emit PaymentReleased(
+                    _retailer,
+                    order.manufacturer,
+                    rawMaterialCost,
+                    remainingPayment
+                );
             }
         }
-        return true;
-    }
-
-    function releasePayment(address[] memory _preOrderBuyers) internal {
-        for (uint256 i = 0; i < _preOrderBuyers.length; i++) {
-            address buyer = _preOrderBuyers[i];
-            uint256 totalEscrowAmount = orders[buyer].escrowBalance;
-            orders[buyer].escrowBalance = 0;
-
-            uint256 rawMaterialCost = totalEscrowAmount / 2;
-            uint256 remainingPayment = totalEscrowAmount - rawMaterialCost;
-
-            address retailerAddress = getRetailerFor(buyer);
-            address manufacturerAddress = getManufacturerFor(buyer);
-
-            payable(retailerAddress).transfer(rawMaterialCost);
-            payable(manufacturerAddress).transfer(remainingPayment);
-
-            emit PaymentReleased(buyer, retailerAddress, manufacturerAddress, rawMaterialCost, remainingPayment);
-        }
-    }
-
-    function getRetailerFor(address _buyer) internal view returns (address) {
-        for (uint256 i = 0; i < retailerPreOrderBuyers[msg.sender].length; i++) {
-            if (retailerPreOrderBuyers[msg.sender][i] == _buyer) {
-                return msg.sender;
-            }
-        }
-        revert("Retailer not found.");
-    }
-
-    function getManufacturerFor(address _buyer) internal view returns (address) {
-        for (uint256 i = 0; i < manufacturerPreOrderBuyers[msg.sender].length; i++) {
-            if (manufacturerPreOrderBuyers[msg.sender][i] == _buyer) {
-                return msg.sender;
-            }
-        }
-        revert("Manufacturer not found.");
     }
 }
